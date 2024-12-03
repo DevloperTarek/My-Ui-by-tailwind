39fae5e9a03d8801cbd7a153e196b003ddab2682
Tarek Hossain <devlopertarek433@gmail.com>
2024-12-03 08:20:30 +0600

    my tailwind first

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// toggle button
+const navMenu = document.querySelector('#nav-menu')
+const navLinks = document.querySelectorAll('.nav-link')
+const hambergetToggleIcon = document.querySelector('#hamber-menu')
+
+hambergetToggleIcon.addEventListener('click',(e)=> {
+     navMenu.classList.toggle('left-[0]')
+    hambergetToggleIcon.classList.toggle('ri-close-large-line');
+})
+
+navLinks.forEach((link)=>{
+     link.addEventListener('click',()=>{
+          navMenu.classList.toggle('left-[0]')
+          hambergetToggleIcon.classList.toggle('ri-close-')
+     })
+});
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // responsive screen for mobile,tablet,desktop,etc
+    screens:{
+      'sm':'340px',
+      'md':'540px',
+      'lg':'768px',
+      'xl':'1170px'
+    },
+    extend: {},
+    // container customize
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:'20px',
+        sm: '10px',
+        lg: '10px',
+        xl: '10px',
+      }
+    },
+    // container customize
+    // font family customize
+    fontFamily:{
+      Jost:['"Jost", sans-serif'],
+      Lobster:['"Lobster", sans-serif']
+    },
+    // font family customize
+    // animation create
+   keyframes:{
+    moveUp:{
+      "50%":{transform:"translateY(-1rem)"}
+    },
+    moveRight:{
+      "0%":{transform:"translateX(0)"},
+      "100%":{transform:"translateX(-10px)"}
+   },
+   moveDown:{
+      '0%':{transform:"rotate(0)"},
+      '100%':{transform:"rotate(360deg)"}
+   },
+  },
+  animation:{
+    animateMoveUp:"moveUp 3s linear infinite",
+    animationMoveRight:"moveRight .9s linear infinite",
+    animateRotate:"moveDown 3s linear infinite"
+   },
+  plugins: [],
+}
+}
